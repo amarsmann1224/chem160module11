a7d6744c9cd6a598f7286a1459a1ca7cf9ac5941
amarsmann1224 <70251359+amarsmann1224@users.noreply.github.com>
2020-10-25 18:31:55 -0700

    Add files via upload

---
+++ regression.R
@@ -0,0 +1,7 @@
+cs<-read.table("cal_sodium.txt",header=T)
+names(cs)
+model<-lm(Calories~Sodium,data=cs)
+plot(Calories~Sodium,data=cs)
+abline(model)
+summary(model)
+# Calories=84.61059+.14319*Sodium
